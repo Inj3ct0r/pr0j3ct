3f6a0373c9861e468a8cf7ad627a7c488eefda6b
m0rt4d3l4 <obobobocrusherofsouls@gmail.com>
2022-09-07 08:50:41 -0300

    Added the new files

---
+++ client/src/components/Validator.js
@@ -0,0 +1,42 @@
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+import { isEmail } from "validator";
+
+const required = value => {
+    if (!value) {
+    return (
+        <div className="alert alert-danger" role="alert">
+        This field is required!
+        </div>
+    );
+    }
+};
+const email = value => {
+    if (!isEmail(value)) {
+    return (
+        <div className="alert alert-danger" role="alert">
+        This is not a valid email.
+        </div>
+    );
+    }
+};
+
+const Validator = () => {
+    return (
+        <Form onSubmit={handleLogin} ref={form}>
+            <Input
+            type="text"
+            className="form-control"
+            
+            validations={[required, email]}
+            />
+            <CheckButton
+            style={{ display: "none" }}
+            ref={checkBtn}
+            />
+        </Form>
+    );
+}
+
+export default Validator;
---
+++ server/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "heffalump.db.elephantsql.com",
+    USER: "yzhjerty",
+    PASSWORD: "ovm6Je61Zh8sRQyn-uVqftJ9mfaJso9I",
+    DB: "yzhjerty",
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+};
